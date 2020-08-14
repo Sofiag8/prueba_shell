5ec910e2bb94e812ae1c34982dbe5871f9649ee6
Sofiag8 <1943@holbertonschool.com>
2020-08-14 00:15:59 +0000

    function that prints the environment

--- supersimpleshell/env-main.c
+++ supersimpleshell/env-main.c
@@ -1,18 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 /**
  * main - prints the environment
  *
  * Return: Always 0.
  */
-char printenv(int ac, char **av, char **env)
+char printenv(char **env)
 {
 	unsigned int i;
+	unsigned int lenght;
 
 	i = 0;
 	while (env[i] != NULL)
 	{
-		printf("%s\n", env[i]);
+		lenght = strlen(env[i]);
+		write(STDOUT_FILENO, env[i], lenght);
+		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
 	return (0);
+}
+void main(int ac, char **av, char **env)
+{
+	printenv(env);
 }
