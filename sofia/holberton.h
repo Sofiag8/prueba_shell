589e9fd964df46637f9d5f29e0f571d14fa6cca6
Sofiag8 <1943@holbertonschool.com>
2020-08-17 15:10:15 +0000

    lskjd

--- sofia/holberton.h
+++ sofia/holberton.h
@@ -19,6 +19,7 @@ char *_strcpy(char *dest, char *src);
 char *_strncat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
+void print_string(char *string);
 
 /* shell basic process */
 int main(int argc, char *argv[], char *env[]);
